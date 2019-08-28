d2c1a02f0d5f72e3be37f0765960933054a0c217
MazurykYaryna <yarynka.mazuryk@gmail.com>
2019-08-28 23:57:47 +0300

    refactoring functionality

--- FL11_HW18_AJAX/js/api.js
+++
@@ -1,3 +0,0 @@
-import fetchData  from './apiHelpers';
-
-export const fetchUsers = () => fetchData('users', 'GET');
--- FL11_HW18_AJAX/js/index.js
+++ FL11_HW18_AJAX/js/index.js
@@ -1,10 +1,12 @@
 const host = 'https://jsonplaceholder.typicode.com/';
 
+//loader
 const toggleLoader = (isShown) => {
     const loader = document.querySelector(".loader")
     isShown ? loader.classList.remove("hidden") : loader.classList.add("hidden");
 }
 
+//api helpers
 const fetchData = (url, method, data = null) => {
     let headers = {
         'Content-Type': 'application/json',
@@ -47,26 +49,62 @@ function sendAsync(url, options) {
     });
 }
 
+//API
 const fetchUsers = () => fetchData('users', 'GET');
-const UpdateUsers = (id, data) => fetchData(`users/${id}`, 'PUT', data);
-const DeleteUser = (id) => fetchData(`users/${id}`, 'DELETE');
-let users1 = [];
+const fetchPosts = (userId) => fetchData(`posts?userId=${userId}`, 'GET');
+const fetchComents = (postId) => fetchData(`comments?postId=${postId}`, 'GET');
+const updateUsers = (id, data) => fetchData(`users/${id}`, 'PUT', data);
+const deleteUser = (id) => fetchData(`users/${id}`, 'DELETE');
 
+//utils 
+const createEl = (element) => document.createElement(element);
 
-DeleteUser(1)
-    .then((user) => console.log(user))
-    .catch(error => console.error(error))
-
+//user list functionality
 const userList = document.querySelector('.userList');
+const wrapper = document.querySelector('.wrapper');
+
+const renderPost = ({ title, body }) => {
+    const postBody = createEl('div');
+    const postTitle = createEl('h5');
+    const postComent = createEl('p');
+
+    postBody.appendChild(postTitle);
+    postBody.appendChild(postComent);
+
+    postBody.classList.add('card-body');
+    postTitle.classList.add('card-title');
+    postComent.classList.add('card-text');
+
+    postTitle.innerHTML = `${title}`;
+    postComent.innerHTML = `${body}`;
+
+    return postBody;
+};
+
+const renderComent = ({ email, body }) => {
+    const comentContainer = createEl('div');
+    const comentTitle = createEl('h6');
+    const comentBody = createEl('p');
+
+    comentTitle.innerHTML = `${email}`;
+    comentBody.innerHTML = `${body}`;
+
+    comentTitle.classList.add('card-subtitle', 'mb-2', 'mt-2', 'text-muted');
+    comentBody.classList.add('card-text');
+
+    comentContainer.append(comentTitle);
+    comentContainer.append(comentBody);
+
+    return comentContainer;
+};
 
 fetchUsers()
     .then(users => {
         users.forEach(user => {
             const myUser = new User(user.name, user.username, user.email, user.phone, user.id);
             const userCard = myUser.renderUser();
-            const editUserCard = myUser.editUser(userCard);
             userList.append(userCard);
-            editUserCard && userList.append(editUserCard);
+            myUser.controlUser(userCard);
         });
     })
     .catch(error => console.error(error))
@@ -79,21 +117,20 @@ class User {
         this.email = email;
         this.phone = phone;
         this.id = id;
-        this.isLoad = false;
+    }
+    redirectOnPrivatePage() {
+
     }
     renderUser() {
-        const doc = document;
-        const createEl = (element) => { return document.createElement(element) }
         const col = createEl('div');
         const card = createEl('div');
         const cardBody = createEl('div');
-        const cardTitle = createEl('p');
+        const cardTitle = createEl('a');
         const cardEmail = createEl('p');
         const cardPhone = createEl('p');
         const deleteButton = createEl('button');
         const editButton = createEl('button');
 
-        cardTitle.setAttribute('href', '');
         col.appendChild(card);
         card.appendChild(cardBody);
         cardBody.appendChild(cardTitle);
@@ -102,14 +139,14 @@ class User {
         cardBody.appendChild(deleteButton);
         cardBody.appendChild(editButton);
 
-        col.classList.add('col-sm-6');
+        col.classList.add('col-sm-6', 'mb-2', 'mt-2');
         card.classList.add('card');
         cardBody.classList.add('card-body');
         cardTitle.classList.add('card-title');
         cardEmail.classList.add('card-text');
         cardPhone.classList.add('card-text');
-        editButton.classList.add('btn', 'btn-primary', 'edit');
-        deleteButton.classList.add('btn', 'btn-primary');
+        editButton.classList.add('btn', 'btn-primary', 'edit', 'ml-1');
+        deleteButton.classList.add('btn', 'btn-primary', 'delete');
 
         cardTitle.innerHTML = `${this.name} ${this.username}`;
         cardEmail.innerHTML = `${this.email}`;
@@ -117,25 +154,49 @@ class User {
         editButton.innerHTML = 'edit user';
         deleteButton.innerHTML = 'delete user';
 
+        cardTitle.addEventListener('click', () => {
+            window.location.replace(`${location.href}#${this.id}`);
+            userList.remove();
+
+            fetchPosts(this.id).then(posts => {
+                posts.forEach((post) => {
+                    const postCard = renderPost(post);
+                    wrapper.append(postCard);
+                    fetchComents(post.id).then(coments => {
+                        coments.forEach(comment => {
+                            const comentContainer = renderComent(comment);
+                            postCard.append(comentContainer);
+                        });
+                    })
+                });
+            });
+        });
+
         return col;
     }
     renderEditForm() {
-        const editCard = document.createElement('div');
-        const editForm = document.createElement('div');
-        const editName = document.createElement('input');
-        const editEmail = document.createElement('input');
-        const editPhone = document.createElement('input');
-        const submit = document.createElement('button');
+        const editCard = createEl('div');
+        const editForm = createEl('div');
+        const editName = createEl('input');
+        const editUserName = createEl('input');
+        const editEmail = createEl('input');
+        const editPhone = createEl('input');
+        const submit = createEl('button');
 
         editCard.classList.add('card');
         editForm.classList.add('card-body', 'form-group');
-        editName.classList.add('form-control');
-        editEmail.classList.add('form-control');
-        editPhone.classList.add('form-control');
+        editName.classList.add('form-control', 'mb-2');
+        editUserName.classList.add('form-control', 'mb-2');
+        editEmail.classList.add('form-control', 'mb-2');
+        editPhone.classList.add('form-control', 'mb-2');
         submit.classList.add('btn', 'btn-primary', 'save');
 
         editName.addEventListener('change', (e) => {
             this.name = e.target.value;
+        });
+
+        editUserName.addEventListener('change', (e) => {
+            this.username = e.target.value;
         });
 
         editEmail.addEventListener('change', (e) => {
@@ -147,10 +208,12 @@ class User {
         });
 
         editName.setAttribute('value', this.name);
+        editUserName.setAttribute('value', this.username);
         editEmail.setAttribute('value', this.email);
         editPhone.setAttribute('value', this.phone);
 
         editName.setAttribute('name', 'name');
+        editUserName.setAttribute('name', 'username');
         editEmail.setAttribute('name', 'email');
         editPhone.setAttribute('name', 'phone');
 
@@ -158,6 +221,7 @@ class User {
 
         editCard.appendChild(editForm);
         editForm.appendChild(editName);
+        editForm.appendChild(editUserName);
         editForm.appendChild(editEmail);
         editForm.appendChild(editPhone);
         editForm.appendChild(submit);
@@ -165,11 +229,11 @@ class User {
         submit.innerHTML = 'Save';
         return editCard;
     }
-    editUser(userCard) {
+    controlUser(userCard) {
         userCard.addEventListener('click', (e) => {
-            this.isLoad = true;
             const isEdit = e.target.className.includes('edit');
             const isSave = e.target.className.includes('save');
+            const isDelete = e.target.className.includes('delete');
             const editForm = this.renderEditForm();
             if (isEdit) {
                 this.removeCard(userCard);
@@ -183,26 +247,26 @@ class User {
                         params[inputs[i].name] = inputs[i].value;
                     }
                 }
-                UpdateUsers(this.id, params)
+                updateUsers(this.id, params)
                     .then(user => {
                         this.removeCard(userCard);
                         this.name = user.name;
+                        this.username = user.username;
                         this.email = user.email;
                         this.phone = user.phone;
-                        this.isLoad = false;
                         this.id = user.id;
                         const currentUpdatedUser = this.renderUser();
                         this.addCard(userCard, currentUpdatedUser);
                     })
                     .catch(err => console.error(err));
             }
-        })
-    }
-    deleteUser() {
-        userCard.addEventListener('click', (e) => {
-            console.log(e.target)
-        })
+            if (isDelete) {
+                deleteUser(this.id)
+                    .then(() => userCard.remove())
+                    .catch(() => console.error(err))
 
+            }
+        })
     }
     removeCard(container) {
         container.firstChild.remove();
